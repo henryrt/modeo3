ea1ebc331f3fa1dbaef0c1d7a8d213bfebb1b253
Ray Henry <ray.henry@alum.mit.edu>
2018-07-21 23:53:00 -0400

    added vs code files

--- LitSched/Program.cs
+++ LitSched/Program.cs
@@ -2,6 +2,8 @@
 using RTH.LiturgySchedule;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Data.Sqlite;
+using System.IO;
+
 
 namespace LitSched
 {
@@ -19,7 +21,21 @@ namespace LitSched
                 .UseSqlite<DataModel>(sb.ConnectionString)
                 .Options;
 
-                DataModel.Initialize(options);
+            DataModel.Initialize(options);
+/* 
+            using (var db = new DataModel(options))
+            {
+                MemoryStream ms = new MemoryStream();
+                var serializer = new DataContractJsonSerializer(typeof(Mass));
+                var masses = db.Masses.ToListAsync().GetAwaiter().GetResult();
+                serializer.WriteObject(ms, masses);
+
+                ms.Position = 0;
+                StreamReader sr = new StreamReader(ms);
+                Console.WriteLine(sr.ReadToEnd());
+
+            }
+ */
         }
     }
 }
